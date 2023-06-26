ccc27db923d7c86ed16410d4160a15a19131e5b2
AKSUF <abu053125@gmail.com>
2023-06-26 20:05:24 +0600

    how to use form events

--- HTML/index.php
+++ HTML/index.php
@@ -5,42 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-      .level{
-        margin-top: 5px;
-      }
-      .result{
-        border: 5px solid green;
-      }
+.scroll{
+  background: #4CAF50;
+  color: white;
+  padding: 15px;
+  width: 50%;
+  height: 50%;
+  overflow: scroll;
+  border: 1px solid #ccc;
+    }
+.scroll1{
+  background: #4CAF50;
+  color: white;
+  padding: 15px;
+  width: 25%;
+  height: 100px;
+  overflow: scroll;
+  border: 1px solid #ccc;
+}
     </style>
 </head>
 <body>
-   <div class="form" id="sform">
-<form action="" method="post">
-    <div class="level mt-4">
-    <label for="name">Name</label>
-    <input type="text" class="form-control" name="sname" id="sname">
-    </div>
-    <div class="level mt-5">
-    <label for="name">Class</label>
-    <input type="text" class="form-control" name="sclass" id="sclass">
-    </div>
-    <div class="level mt-5">
-    <label for="name">Country</label>
-    <select name="scountry" id="scountry" class="form-control">
-        <option value="dhaka">Dhaka</option>
-        <option value="dhaka">Delihi</option>
-        <option value="dhaka">Sunamgonj</option>
-    </select>
-    </div>
-<div>
-    <input type="submit" value="Submit" class="m-auto">
+<div class="scroll">
+<div class="scroll1">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, minima. Est architecto impedit vel necessitatibus blanditiis rerum iure laboriosam! Qui aut esse eos delectus dolorum, ipsum asperiores voluptates ut corporis.
 </div>
-</form>
-
-   </div>
-
-<div class="result" id="result"></div>
-
+<br><br>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem distinctio nisi molestias accusantium magni alias delectus possimus qui quas architecto! Ab laboriosam quaerat obcaecati iste quisquam, ipsum vitae dolorem minima?</p>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 <script src="../JS/index.js"></script>
 
--- JS/index.js
+++ JS/index.js
@@ -1,32 +1,9 @@
 $(document).ready(function() {
-    $('#sname,#sclass,#scountry').focus(function() {
-        $(this).css('background-color', 'lime');
-    });
+    $('#box').scroll(function() {
+        console.log("You are scrolling");
+    })
 
-    $('#sname,#sclass,#scountry').blur(function() {
-        $(this).css('background-color', '');
-    });
-
-    $('#sname,#sclass,#scountry').change(function() {
-        $(this).css('background-color', '');
-    });
-
-    $('#scountry').change(function() {
-        $(this).css('background-color', 'pink');
-    });
-
-    $('#scountry').change(function() {
-        var a = $(this).val();
-        $('#result').html(a);
-    });
-
-    $('#sname,#sclass').change(function() {
-        $(this).css('background-color', '');
-    });
-
-    $('#sform').submit(function() {
-        console.log("Form subiiteed suuccessfully");
-    });
-
-
+    $(window).resize(function() {
+        console.log("You are resizing window");
+    })
 });
