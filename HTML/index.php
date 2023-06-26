0d4966d76f64c0b546133b3ee0496a2c8175ed48
AKSUF <abu053125@gmail.com>
2023-06-26 17:15:53 +0600

    how to use event function

--- HTML/index.php
+++ HTML/index.php
@@ -5,38 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        .box{
+        .test{
             width: 50%;
             margin-top: 4px;
             margin: auto;
             border:solid 2px red;
             border-top-left-radius: 2px;
         }
-        .box p{
-            color: red;
-        }
+    
     </style>
 </head>
 <body>
-    <div class="box">
+    <div id="box" class="test">
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi recusandae saepe, at quod quas itaque quisquam nesciunt officia sit velit perspiciatis vero illum doloribus nemo nostrum repellendus doloremque iure a!</p>
-
     </div>
 
-<ul id="list">
-    <li>Potato</li>
-    <li>Cucumber</li>
-    <li>Jackfruit</li>
-</ul>
-
-
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 <script src="../JS/index.js"></script>
-
 
 </body>
 </html>
--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-    alert(1);
+    $('#box').click(function() {
+        $('#box').css('background-color', 'green');
+    });
 });
-
 $(document).ready(function() {
-    // $('#list li').css("border", "2px solid red");
-    // $('li:last-child').css("border", "2px solid red");
-    // $('ul li:eq(2)').css("border", "2px solid red");
-    $('ul li:gt(1)').css("border", "2px solid red");
-})
+    $('#box').dblclick(function() {
+        $('#box').css('background-color', 'orange');
+    });
+});
+$(document).ready(function() {
+    $('#box').contextmenu(function() {
+        $('#box').css('background-color', 'blue');
+    });
+});
+$(document).ready(function() {
+    $('#box').mouseenter(function() {
+        $('#box').css('background-color', '#646A3E');
+    });
+});
+$(document).ready(function() {
+    $('#box').mouseleave(function() {
+        $('#box').css('background-color', '#691677');
+    });
+});
