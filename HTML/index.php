13e02ea54a3efdc8f7f849ff4628601a820dd5c5
AKSUF <abu053125@gmail.com>
2023-06-27 14:26:11 +0600

    how to use box

--- HTML/index.php
+++ HTML/index.php
@@ -1,39 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        .box {
-            border: 6px solid green;
-            width: 150px;
-            height: 50%;
+        .mainouter {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+        }
+        
+        .outer {
+            width: 400px;
+            height: 400px;
+            border: 2px solid black;
+            margin: 10px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        
+        .inner {
+            width: 200px;
+            height: 160px;
+            border: 2px solid red;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
             position: relative;
         }
-        .box2 {
-            border: 6px solid green;
-            margin-top: 5px;
-        }
-        #boxbg {
-            background-color: orange;
-        }
-        #hasclassBtn{
-            margin-top: 50px;
+        
+        .first {
+            width: 50px;
+            height: 20px;
+            border: 2px solid blue;
+            margin: 5px;
         }
     </style>
 </head>
+
 <body>
-    <div class="box" id="box" >
-   <h1>Scrolling measure</h1>
-   <p>This is a topic</p>
-<div class="box2" id="box2"></div>
+    <div class="mainouter" id="mainouter">
+        <p>mainouter</p>
+        <div class="outer" id="outer">
+            <p>outer</p>
+            <div class="inner" id="inner">
+                <p>inner</p>
+                <div class="first">A</div>
+                <div class="first">B</div>
+                <div class="first" id="child-C">C</div>
+                <div class="first">D</div>
+            </div>
+        </div>
     </div>
-  <div class="topic"></div>
-    <button id="animatedBtn">AnimatedBTn</button>
-    <button id="stopBtn">Stop</button>
- 
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     <script src="../JS/index.js"></script>
 </body>
+
 </html>
--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $('#animatedBtn').click(function() {
-        $('#box').css('background', 'pink').slideUp(1000).slideDown(1000);
-
-    })
+    // $('#inner').parents('.mainouter').css('background', 'red');
+    // $('#inner').parentsUntil('#mainouter').css('background', 'red');
+    // $('#inner').offsetParent('#mainouter').css('background', 'red');
+    $('#inner').closet('div').css('background', 'red');
 
 });
