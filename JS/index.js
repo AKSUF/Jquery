fffd805dd453f675b88cc798bc82307ebe70c8bf
AKSUF <abu053125@gmail.com>
2023-06-27 16:13:47 +0600

    how to find in children

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    // $('#inner').parents('.mainouter').css('background', 'red');
-    // $('#inner').parentsUntil('#mainouter').css('background', 'red');
-    // $('#inner').offsetParent('#mainouter').css('background', 'red');
-    $('#inner').closet('div').css('background', 'red');
-
+    // $("#inner").children().css('background', 'red');
+    // $("#outer").children().css('background', 'red');
+    $("#inner").find('.first').css('background', 'red');
 });
