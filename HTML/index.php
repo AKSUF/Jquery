d49a52cd74b0b7a571b008c5e8f0f6823fdb78b9
AKSUF <abu053125@gmail.com>
2023-06-27 17:23:01 +0600

    how to use each method

--- HTML/index.php
+++ HTML/index.php
@@ -6,40 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-   #first{
-    background-color: red;
-    border: 3px solid green;
-   }
-   #second{
-    background-color: blue;
-    border: 2px solid navy;
-   }
-   
+        #first {
+            background-color: red;
+            border: 3px solid green;
+        }
+
+        #second {
+            background-color: blue;
+            border: 2px solid navy;
+        }
     </style>
 </head>
 
 <body>
-   <h1>Jquery Filtering Methods</h1>
-   <div id="first">
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-<p class="test">Lorem ipsum dolor sit amet.</p>
-</div>
-<ul>
-<li>Orange</li>
-<li><span>Apple</span></li>
-<li>Banana</li>
-<li>Grapes</li>
-<li>Coconut</li>
-</ul>
+    <h1>Jquery Filtering Methods</h1>
+    <div id="first">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+        <p class="test">Lorem ipsum dolor sit amet.</p>
+    </div>
 
-<div id="second">
-    <p id="test">
-        Lorem ipsum <span class="test">Lorem, ipsum dolor.</span> dolor sit amet consectetur adipisicing elit.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur.</p>
-</div>
+    <ul>
+        <li>Orange</li>
+        <li><span>Apple</span></li>
+        <li>Banana</li>
+        <li>Grapes</li>
+        <li>Coconut</li>
+    </ul>
 
-   
+    <div id="second">
+        <p id="test">
+            Lorem ipsum <span class="test">Lorem, ipsum dolor.</span> dolor sit amet consectetur adipisicing elit.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+
+    <div id="demo"></div>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     <script src="../JS/index.js"></script>
 </body>
--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,6 @@
 $(document).ready(function() {
-    $('p').has('.test').css('background', 'gold');
-
-    if ($('p').parent().is('div')) {
-        console.log($('p').parent().is('div'));
-    }
-
-    $('p').click(function() {
-        if ($(this).parent().is('div')) {
-            $(this).css('background', 'gold');
-        }
-    })
-
+    $('li').each(function() {
+        $('#demo').html($(this).text());
+        console.log($(this).text());
+    });
 });
