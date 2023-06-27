cc1ff05c2240ce97a6fd4701e44bb4dc1feb3e17
AKSUF <abu053125@gmail.com>
2023-06-27 19:46:15 +0600

    how to use plugin

--- HTML/index.php
+++ HTML/index.php
@@ -6,44 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        #first {
-            background-color: red;
-            border: 3px solid green;
-        }
-
-        #second {
-            background-color: blue;
-            border: 2px solid navy;
-        }
-        #result {
-            background-color: blanchedalmond;
-            margin: auto;
-            width: 30%;
-        }
-        #box {
-  width: 200px;
-  height: 200px;
-  background-color: #eaeaea;
-  border: 2px solid #333;
+   h1{
+    text-align: center;
+    color: rebeccapurple;
+   }
+#scrollUp{
+    bottom:20px;
+    right:20px;
+    padding: 10px 20px;
+    background-color: #555;
+    color: #fff;
 }
 
     </style>
 </head>
 
 <body>
-<h1>Jquery Filtering Methods</h1>
-<div id="box">
-    <h1>Some Heading</h1>
- <p>Lorem ipsum dolor sit.</p>
- <p>Lorem ipsum dolor sit.</p>
- <p>Lorem ipsum dolor sit.</p>
+<h1>Jquery Scrollup plugin</h1>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt, adipisci. Accusamus sit facere cupiditate culpa cum dolor aliquam totam tenetur porro. Mollitia nesciunt saepe reprehenderit eveniet voluptate odio doloremque veniam labore dicta eum nobis quas aut ipsa vitae ducimus consequatur rerum debitis totam possimus, temporibus, deserunt a harum quos. Et, distinctio nemo. Cum excepturi qui et enim rerum omnis, eaque ad voluptatibus earum non, optio, placeat quisquam quae asperiores perspiciatis similique ipsa ratione dolor architecto eum delectus beatae maiores repellendus atque. Consectetur excepturi aliquid magnam animi voluptate voluptatum ad nisi asperiores, cumque omnis quaerat quasi laborum illo neque impedit? Totam consequatur porro aliquam hic. Blanditiis perferendis nostrum beatae asperiores similique fugiat laborum provident saepe ducimus optio quod nesciunt esse, iste autem earum suscipit impedit sequi voluptatem quasi repudiandae ut nulla ratione? Animi quisquam omnis impedit quibusdam vero, incidunt saepe quaerat? Deleniti officiis architecto ipsum ut sed distinctio hic voluptas, maxime similique nihil reprehenderit explicabo veniam voluptate iste vel, rem quasi. Natus sequi minus perferendis mollitia sapiente enim illo exercitationem expedita laborum. Neque eius et autem. Vero doloribus repellat, expedita nulla in alias quidem ratione nam voluptatem dicta corporis ullam exercitationem culpa aliquam blanditiis, ut accusantium, quam voluptate voluptates reiciendis magnam ab aspernatur officiis. Omnis excepturi eveniet laudantium possimus laborum! Blanditiis mollitia eligendi laudantium! Necessitatibus eius accusantium beatae, molestias, deleniti quas impedit libero minima molestiae soluta ipsam aperiam rerum tempora unde nisi perferendis qui quo ipsa deserunt voluptatem distinctio tempore debitis! Voluptatibus cumque quaerat, fugit excepturi sint deleniti! Optio assumenda nobis eveniet quasi expedita nisi ipsa. Exercitationem molestiae perspiciatis praesentium earum eveniet facilis perferendis dolorum animi dolorem laborum quam nobis odio cum amet suscipit iusto inventore totam ducimus, obcaecati eum corrupti quos natus dolores omnis. Perferendis a neque, sapiente, eveniet similique ipsa quia et vero voluptatem totam itaque cupiditate consectetur in ut impedit nam amet? Eos incidunt recusandae veniam facilis. Enim soluta sint quasi, autem voluptates iure ullam tempore nihil pariatur excepturi dolorem deleniti, accusantium fuga quaerat repellendus officia saepe magnam possimus optio nisi. Doloremque modi eum ipsum nostrum itaque minus illum id deserunt voluptatibus eos dolorum sit odio vitae laborum iure exercitationem sed ex vel reprehenderit autem, doloribus hic perferendis corrupti ut? Odit repellat sit ipsam. Nemo praesentium quod porro autem? Voluptatibus dicta maiores rem nobis dignissimos incidunt facere, deleniti reiciendis aperiam labore, ad delectus odit molestias est aut recusandae quidem dolorem, culpa vitae eum ut enim. Repellat, sit error sint inventore similique corporis, distinctio omnis sunt quasi laboriosam excepturi. Quisquam aliquam voluptatibus maiores libero asperiores impedit dolorum tenetur ab nihil totam! Provident reprehenderit tenetur repudiandae eos accusamus illum eveniet ex obcaecati sed totam molestiae qui illo quos nostrum quod, impedit sit voluptas natus ipsum doloremque tempora nisi facilis magni! Mollitia exercitationem facilis magnam laudantium, obcaecati molestiae veniam reiciendis cum incidunt error ad voluptatibus earum non veritatis rem harum quidem laborum necessitatibus, culpa tempore nisi. Inventore qui culpa fuga mollitia doloremque eos corporis fugit commodi quod dignissimos sit ipsam quo praesentium blanditiis quibusdam cum deleniti exercitationem nisi, iste error. Ab eaque non corporis sequi? Sed corporis facere ut laudantium itaque illum impedit nobis, saepe officia delectus obcaecati. Eos ipsa officia id rerum minus sed vel error molestias reprehenderit beatae, excepturi a consequuntur delectus quae est, esse unde dolore. Ipsa provident cum magnam magni fuga voluptatibus ducimus. Ad laudantium quo asperiores dolor nulla quis provident aliquid nihil suscipit! Asperiores eum amet aliquid nihil, maxime cumque. Dolorem minima assumenda ullam culpa consequuntur voluptates nam cum a architecto! Vero nihil unde, ea iure animi aut vel blanditiis doloremque nesciunt, dicta molestias, praesentium cum voluptas laudantium repellendus veniam asperiores perspiciatis quasi quod molestiae neque?</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, ea ex ipsa, ut consequuntur quo autem commodi magnam modi, eius eveniet dolore ducimus id facilis suscipit reiciendis asperiores minima libero officia ab nisi quas ipsum. Sapiente suscipit facere veritatis officia hic adipisci cum unde atque. Explicabo atque provident blanditiis, quod ab voluptate at odit dolores! Doloremque corporis accusantium ipsum, voluptatum consequuntur iste officiis soluta modi enim cum dicta numquam dolorem eligendi dolore deleniti perspiciatis. Similique sint voluptatem modi pariatur, quia, deleniti rem corporis a praesentium iste eaque dolor nemo quisquam quae, aliquid voluptates quos! Eveniet nostrum ex recusandae molestiae libero quasi magni, quis laborum, qui numquam corrupti repellat quam ad voluptates amet non culpa porro excepturi necessitatibus inventore! Magnam dolores eos, asperiores maxime minus temporibus enim quis facere aliquid soluta voluptatibus voluptatem, cumque nobis quaerat, velit nisi obcaecati debitis id commodi eum beatae ducimus voluptate? Possimus cumque, ex sequi amet explicabo sed ducimus ipsum ratione saepe distinctio asperiores necessitatibus nobis nesciunt atque voluptatibus voluptate. Itaque corrupti animi similique eos. Assumenda nihil, similique quia quae, perspiciatis, asperiores ducimus autem obcaecati totam provident dignissimos rerum sit aut aperiam quasi quos deserunt quas cumque voluptates? Culpa perferendis odit laudantium temporibus commodi, reprehenderit nostrum rerum dolorum, repellendus optio dolor esse nesciunt aspernatur quae nulla quis aliquid sit exercitationem. Asperiores ut est ullam illo optio quos at, quisquam omnis amet nisi quae delectus, deserunt exercitationem illum doloribus vero suscipit repellat obcaecati? Natus placeat vel dolores reiciendis libero minus animi ipsum, harum ducimus corporis eum molestias laudantium est commodi! Dignissimos ipsam excepturi voluptatem veritatis dolores rem, nemo possimus commodi, dolorum itaque qui debitis quasi nulla quis. Perferendis eum magnam, laborum reprehenderit autem mollitia quaerat soluta dolore unde quisquam impedit quos voluptates nemo libero sequi odit illo vel repudiandae ducimus aperiam exercitationem a at. Nemo repudiandae molestias sed eum, voluptatem porro fugiat nisi consectetur fuga corporis, pariatur aliquid corrupti, dolorem nesciunt placeat non eveniet sapiente reiciendis? Vero nemo eum pariatur voluptatibus quasi dicta debitis fuga ipsa voluptates, vitae quas cupiditate tempora et quam asperiores labore. Blanditiis laborum architecto aliquam. Magnam ipsam eligendi in voluptate nobis veritatis? Repudiandae doloribus dolore, fuga labore repellendus eveniet, inventore cum natus placeat illum ex ducimus debitis nemo qui? Officia amet adipisci obcaecati rerum possimus ab voluptate assumenda placeat praesentium quisquam dolorum deserunt, maxime consectetur, unde quae! Unde incidunt dignissimos hic explicabo eveniet molestias reprehenderit vero possimus exercitationem nihil velit repellendus nemo at quas quae, excepturi fugit eum delectus ipsa minus laboriosam. Quisquam vitae itaque tempore, veritatis eos porro sapiente iusto libero officiis natus excepturi nisi nostrum asperiores rem similique culpa sed numquam! Aut odio tempore dicta hic expedita dignissimos porro rerum odit nulla! Consectetur, similique magnam! Dolor a voluptatibus et at suscipit sit exercitationem ratione iste quia alias, quis consectetur, accusantium dolore similique in repellendus, quam reiciendis aliquam beatae repudiandae doloribus? Sed optio blanditiis minus porro corporis quidem laboriosam modi ipsa eos obcaecati quasi voluptatum adipisci nulla non itaque, perspiciatis pariatur a. Est, quas voluptate libero, laboriosam ea cupiditate amet esse reprehenderit, aliquid aut incidunt possimus praesentium nesciunt natus ullam nam illum consectetur dolorem? Sapiente illum officiis exercitationem molestias? Cum eligendi ex sint, molestias, sed dolorum dolor dicta odio quos laudantium, itaque incidunt numquam porro autem iusto enim totam! Cumque repudiandae nihil est. Natus adipisci recusandae distinctio rerum quae praesentium assumenda vel quis est dolorem nostrum eligendi, possimus error aperiam id laudantium facere magni. Quisquam non expedita eaque dignissimos excepturi officiis dolores reiciendis quae voluptas? Dignissimos eum quo natus blanditiis ullam ut, a amet totam vel saepe in. Tenetur eum qui cum, quae eaque ut voluptatum beatae totam fugiat. Officia, aperiam sint.</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa repudiandae, corrupti debitis neque ab aliquam quibusdam fugit voluptates sunt ipsum doloremque necessitatibus est voluptate alias ratione aspernatur? Ea fugit placeat optio id, perferendis commodi eius tenetur ipsa nihil explicabo! Odit earum deleniti ea! Doloribus commodi exercitationem excepturi repellat, minima ut voluptatibus harum ipsa saepe consectetur aliquid distinctio maiores corporis, iste error, magnam sit assumenda a quam. Sint voluptatem cumque ex veniam aperiam iure quo delectus quas sapiente minus veritatis odit earum neque temporibus sit tempore asperiores quisquam id, est quibusdam mollitia vel! Illum, eius quasi veniam accusamus nulla quis sequi libero amet, ad recusandae optio ullam tenetur omnis veritatis nostrum maiores architecto? Laborum eligendi consectetur ratione et magni nam officia dolor tempora, cumque dolore ipsum sunt, cupiditate nisi assumenda provident distinctio, culpa repudiandae recusandae quia eaque necessitatibus! Magnam quos, non fuga porro rem adipisci eaque officiis placeat. Eaque harum similique saepe sed minima et incidunt, magnam voluptate delectus iure soluta doloremque expedita enim, aut dolorum nihil quibusdam dicta vel fuga quis. Odio, assumenda iure nostrum fugit aperiam sed dolores, vero accusantium tenetur explicabo vitae repellat commodi? Voluptatem libero deleniti eligendi ad ratione, accusamus minima incidunt expedita in! Neque, necessitatibus perferendis!</p>
 
-</div>
-<br><br>
-<h2></h2>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 <script src="../JS/index.js"></script>
+<script src="../plugin/jquery.scrollUp.min.js"></script>
+<script>
+$(function () {
+    $.scrollUp({
+        scrollName: 'scrollUp',      // Element ID
+        scrollDistance: 100,         // Distance from top/bottom before showing element (px)
+        scrollFrom: 'top',           // 'top' or 'bottom'
+        scrollSpeed: 300,            // Speed back to top (ms)
+        easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+        animation: 'fade',           // Fade, slide, none
+        animationSpeed: 200,         // Animation speed (ms)
+        scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
+        scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
+        scrollText: 'Click to go up', // Text for element, can contain HTML
+        scrollTitle: false,          // Set a custom <a> title if required.
+        scrollImg: false,            // Set true to use image
+        activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        zIndex: 2147483647    
+    });
+});
+</script>
 </body>
 
 </html>
